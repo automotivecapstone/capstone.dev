3132a48954873f1207d00a33dbe621b19162d52c
pascalallen <thomaspascalallen@yahoo.com>
2016-01-19 15:47:48 -0600

    end of day commit. no change.

--- app/views/tutorials/edit.blade.php
+++ app/views/tutorials/edit.blade.php
@@ -13,7 +13,7 @@
 		{{-- <div class="subheader">Blog Stuffz</div> --}}
 		<hr>
 
-		{{ Form::model($tutorial, array('action' => array('TutorialsController@update', $tutorial->id), 'method' => 'PUT')) }}
+		{{ Form::model($tutorial, array('action' => array('TutorialsController@update', $tutorial->id), 'method' => 'PUT', 'files' => true)) }}
 
 			<div class="form-group {{ ($errors->has('title')) ? 'has-error' : '' }}">
 				{{ $errors->first('title', '<div class="alert alert-danger">:message</div>') }}
@@ -30,7 +30,8 @@
 			<div class="form-group {{ ($errors->has('image')) ? 'has-error' : '' }}">
 				{{ $errors->first('image', '<div class="alert alert-danger">:message</div>') }}
 				{{ Form::label('image', 'Image') }}
-				<input type="text" class="form-control" id="image" name="image" value="{{{ $tutorial->image }}}"></input>
+				{{{ $tutorial->image }}}
+				{{ Form::file('image') }}
 			</div>
 
 			<button type="submit" class="btn btn-default">Submit</button>
