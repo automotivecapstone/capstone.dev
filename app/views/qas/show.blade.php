83ad06e7e0bbc1f59812e07ff0aaf9e354ef9a1f
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2016-01-19 14:22:18 -0600

    added delete button

--- app/views/qas/show.blade.php
+++ app/views/qas/show.blade.php
@@ -1,1 +1,19 @@
-/vagrant/sites/capstone.dev/app/views/qas/show.blade.php
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+	<h1>{{$qa->question}}</h1>
+	<h3>{{$qa->body}}</h3>
+	<p>{{$qa->image}}</p>
+	<p>Created: {{{$qa->created_at->diffForHumans() }}}</p>
+	<button type="submit" class="btn btn-default">	
+		<a href="{{{ action('QasController@edit', $qa->id) }}}">Edit this Question</a>
+	</button>
+	{{ Form::open(array('action' => array('QasController@destroy', $qa->id, 'files' => true), 'method' => 'DELETE')) }}
+		<button class="delete-button btn btn-danger">Delete Post</button>
+	{{ Form::close() }}
+	
+</div>
+
+@stop
