65a8960953e141eef48c2325163d05a1e1204dc2
pascalallen <thomaspascalallen@yahoo.com>
2016-01-19 14:06:29 -0600

    Tutorial directory has basic functionality. Create, edit, index, show are all working.

--- app/models/Tutorial.php
+++ app/models/Tutorial.php
@@ -2,13 +2,17 @@
 
 class Tutorial extends \Eloquent {
 
+    protected $table = 'tutorials';
+
 	// Add your validation rules here
-	public static $rules = [
-		// 'title' => 'required'
-	];
+	public static $rules = array(
+	    'title'      => 'required|min:2|max:100',
+	    'content'    => 'required|min:2|max:10000',
+	    'image'		 => 'image'
+	);
 
 	// Don't forget to fill this array
-	protected $fillable = [];
+    protected $fillable = array('title', 'content', 'image');
 
 	public function user()
     {
--- app/views/tutorials/edit.blade.php
+++ app/views/tutorials/edit.blade.php
@@ -1,1 +1,34 @@
-/vagrant/sites/capstone.dev/app/views/tutorials/edit.blade.php
+@extends('layouts.master')
+
+@section('top-script')
+
+	{{-- CUSTOM CSS --}}
+	{{-- <link rel="stylesheet" type="text/css" href="/css/tutorials-edit.css"> --}}
+	
+@stop
+
+@section('content')
+	<div class="container">
+		<div class="header">Edit Tutorial</div>
+		{{-- <div class="subheader">Blog Stuffz</div> --}}
+		<hr>
+
+		{{ Form::model($tutorial, array('action' => array('TutorialsController@update', $tutorial->id), 'method' => 'PUT')) }}
+
+			<div class="form-group {{ ($errors->has('title')) ? 'has-error' : '' }}">
+				{{ $errors->first('title', '<div class="alert alert-danger">:message</div>') }}
+				{{ Form::label('title', 'Title') }}
+				<input type="text" class="form-control" id="title" name="title" value="{{{ $tutorial->title }}}">
+			</div>
+
+			<div class="form-group {{ ($errors->has('content')) ? 'has-error' : '' }}">
+				{{ $errors->first('content', '<div class="alert alert-danger">:message</div>') }}
+				{{ Form::label('content', 'Content') }}
+				<input type="text" class="form-control" id="content" name="content" value="{{{ $tutorial->content }}}"></input>
+			</div>
+
+			<button type="submit" class="btn btn-default">Submit</button>
+			
+		{{ Form::close() }}
+	</div>
+@stop
