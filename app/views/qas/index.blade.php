3f7c0c31f7cdf13ad36132b51cb038baff51e832
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2016-01-19 14:21:33 -0600

    created blade and working on functionality - not working

--- app/views/qas/index.blade.php
+++ app/views/qas/index.blade.php
@@ -2,9 +2,21 @@
 
 @section('content')
 
-<div>
-	<h2>Index - Q & A</h2>
-</div>
+
 <div class="container">
 
+	@foreach($qas as $qa)
+    <div class="col-lg-12">
+        <hr>
+        <h2 class="intro-text text-center">
+        	<a href="{{{ action('QasController@show', $qa->id) }}}">~ {{{ $qa->question }}} </a>
+        	<a href="{{{ action('PostsController@show', $post->id) }}}">{{{ $qa->body }}}</a>
+        </h2>
+        <hr>
+    </div>
 </div>
+
+	{{ $qas->links() }}
+
+
+@stop
