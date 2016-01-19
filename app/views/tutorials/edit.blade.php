b82e8a99c8bdeb7199b16830d6f555c9552f6d55
pascalallen <thomaspascalallen@yahoo.com>
2016-01-19 15:24:28 -0600

    Working on tutorials views.

--- app/views/tutorials/edit.blade.php
+++ app/views/tutorials/edit.blade.php
@@ -3,7 +3,7 @@
 @section('top-script')
 
 	{{-- CUSTOM CSS --}}
-	{{-- <link rel="stylesheet" type="text/css" href="/css/tutorials-edit.css"> --}}
+	<link rel="stylesheet" type="text/css" href="/css/tutorials-edit.css">
 	
 @stop
 
@@ -25,6 +25,12 @@
 				{{ $errors->first('content', '<div class="alert alert-danger">:message</div>') }}
 				{{ Form::label('content', 'Content') }}
 				<input type="text" class="form-control" id="content" name="content" value="{{{ $tutorial->content }}}"></input>
+			</div>
+
+			<div class="form-group {{ ($errors->has('image')) ? 'has-error' : '' }}">
+				{{ $errors->first('image', '<div class="alert alert-danger">:message</div>') }}
+				{{ Form::label('image', 'Image') }}
+				<input type="text" class="form-control" id="image" name="image" value="{{{ $tutorial->image }}}"></input>
 			</div>
 
 			<button type="submit" class="btn btn-default">Submit</button>
