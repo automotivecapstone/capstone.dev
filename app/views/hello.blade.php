a01feb1f15a61d061a3ca01cf6db2c02712001e4
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2016-01-21 15:03:44 -0600

    testing styles on landing page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -4,13 +4,14 @@
 
 	{{-- CUSTOM CSS BELOW --}}
 	<link href="/css/hello.css" type="text/css" rel="stylesheet">
+	<link href="/css/general-site.css" type="text/css" rel="stylesheet">
 
 @stop
 
 @section('content')
 
-	<h1>Welcome to our site!</h1>
-
+	<h1 class="font-magneto">Grease Monkey</h1>
+	<h1 class="font-monkey">Grease Monkey</h1>
 @stop
 
 @section('top-script')
