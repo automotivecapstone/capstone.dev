1d3a627758935e30f6c9caa38f63f93a8e1e652d
pascalallen <thomaspascalallen@yahoo.com>
2016-01-25 12:52:55 -0600

    Fixed edit forms for both qa show and tutorial show. Shows current image and video and user can choose to upload a new one.

--- app/views/qas/edit.blade.php
+++ app/views/qas/edit.blade.php
@@ -21,12 +21,21 @@
 	<div class="form-group  {{ ($errors->has('image')) ? 'has-error' : ''}}">
 		{{ $errors->first('image', '<div class="alert alert-danger">:message</div>') }}
 		{{ Form::label('image', 'Image') }}
+		<p>Current Image:</p>
 		{{{ $qa->image }}}
 		{{ Form::file('image') }}
 	</div>
 
+	<div class="form-group {{ ($errors->has('video')) ? 'has-error' : '' }}">
+        {{ $errors->first('video', '<div class="alert alert-danger">:message</div>') }}
+        {{ Form::label('video', 'Video') }}
+        <p>Current Video:</p>
+        {{{ $qa->video }}}
+        {{ Form::file('video', null) }}
+    </div>
+
   	<button type="submit" class="btn btn-default">Submit Changes</button>
-  	
+
 {{ Form::close() }}
 
 @stop
--- app/views/tutorials/edit.blade.php
+++ app/views/tutorials/edit.blade.php
@@ -21,9 +21,18 @@
 	<div class="form-group {{ ($errors->has('image')) ? 'has-error' : '' }}">
 		{{ $errors->first('image', '<div class="alert alert-danger">:message</div>') }}
 		{{ Form::label('image', 'Image') }}
+		<p>Current Image:</p>
 		{{{ $tutorial->image }}}
 		{{ Form::file('image') }}
 	</div>
+
+	<div class="form-group {{ ($errors->has('video')) ? 'has-error' : '' }}">
+        {{ $errors->first('video', '<div class="alert alert-danger">:message</div>') }}
+        {{ Form::label('video', 'Video') }}
+        <p>Current Video:</p>
+        {{{ $tutorial->video }}}
+        {{ Form::file('video', null) }}
+    </div>
 
 	<button type="submit" class="btn btn-default">Submit</button>
 	
