188a748c8bb46b829d520394f8075e142cee799e
pascalallen <thomaspascalallen@yahoo.com>
2016-01-25 12:43:25 -0600

    Fixed qas.edit view and functionality. Making progressgit add -A YAYgit add -A.

--- app/views/qas/edit.blade.php
+++ app/views/qas/edit.blade.php
@@ -5,22 +5,28 @@
 <p class="logo">Edit Your Question</p>
 
 {{ Form::model($qa, array('action' => array('QasController@update', $qa->id), 'method' => 'PUT')) }}
+
 	<div class="form-group {{ ($errors->has('question')) ? 'has-error' : ''}}">
+		{{ $errors->first('question', '<div class="alert alert-danger">:message</div>') }}
 		{{ Form::label('question', 'Question') }}
-		{{ Form::text('question', null, ['class' => 'form-control', 'placeholder' => 'Enter your blog question']) }}
+		<input type="text" class="form-control" id="question" name="question" value="{{{ $qa->question }}}">
 	</div>
 
 	<div class="form-group {{ ($errors->has('content')) ? 'has-error' : ''}}">
+		{{ $errors->first('content', '<div class="alert alert-danger">:message</div>') }}
 		{{ Form::label('content', 'Content') }}
-		{{ Form::text('content', null, ['class' => 'form-control', 'placeholder' => 'Enter your content']) }}
+		<input type="text" class="form-control" id="content" name="content" value="{{{ $qa->content }}}"></input>
 	</div>
 
 	<div class="form-group  {{ ($errors->has('image')) ? 'has-error' : ''}}">
+		{{ $errors->first('image', '<div class="alert alert-danger">:message</div>') }}
 		{{ Form::label('image', 'Image') }}
-		{{ Form::textarea('image', null, ['class' => 'form-control', 'placeholder' => 'Enter your image']) }}
+		{{{ $qa->image }}}
+		{{ Form::file('image') }}
 	</div>
 
   	<button type="submit" class="btn btn-default">Submit Changes</button>
+  	
 {{ Form::close() }}
 
 @stop
