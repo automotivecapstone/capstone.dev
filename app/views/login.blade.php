b019a3b0a7a93fef05c731566e0244fac1fc45b1
MK <mkwarren21@gmail.com>
2016-01-26 09:58:03 -0600

    fixed login layout issues

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,27 +4,27 @@
 
 @section('content')
 
-<div class="container">
+	<p class="logo">log in</p>
+	<div class="login-form-1">
+		{{ Form::open(array('action' => 'HomeController@postLogin')) }}
 
-	<p class="logo">Log In</p>
-		<div class="login-form-1">
-			{{ Form::open(array('action' => 'HomeController@postLogin')) }}
 
-			<div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
-				{{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
-				{{ Form::label('email', 'Email') }}
-				{{ Form::text('email', Input::old('email'), array('class' => 'form-control')) }}
-			</div>
+		<div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
+			{{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
+			{{ Form::label('email', 'Email') }}
+			{{ Form::text('email', Input::old('email'), ['class' => 'form-control', 'placeholder'=>'Enter your email']) }}
+		</div>
 
-			<div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
-				{{ $errors->first('password', '<div class="alert alert-danger">:message</div>') }}
-				{{ Form::label('password', 'Password') }}
-				{{ Form::password('password', array('class' => 'form-control')) }}
-				<br>
-				{{ Form::submit('submit') }}
-				{{ Form::close() }}
+		<div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
+			{{ $errors->first('password', '<div class="alert alert-danger">:message</div>') }}
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password', ['class' => 'form-control', 'placeholder'=>'Enter your password']) }}
+			<br>
+			{{ Form::submit('submit') }}
+			{{ Form::close() }}
 
-			</div>
-
+		</div>
 	</div>
+
+
 @stop
