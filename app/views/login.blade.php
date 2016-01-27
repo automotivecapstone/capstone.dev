35290b501dbb335957a63e2f4314ac01732362ea
MK <mkwarren21@gmail.com>
2016-01-27 10:19:45 -0600

    updating login form

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,25 +4,28 @@
 
 @section('content')
 
-	<p class="logo">log in</p>
-	<div class="login-form-1">
-		{{ Form::open(array('action' => 'HomeController@postLogin')) }}
+	
+	<div class="form-main">
+		<div class ="form-div">
+			{{ Form::open(array('action' => 'HomeController@postLogin'), ['class'=>'form', 'id'=>'form1']) }}
 
+			<p class = 'font-magneto text-center'>log in</p>
 
-		<div class="form-group {{ ($errors->has('email')) ? 'has-error' : '' }}">
-			{{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
-			{{ Form::label('email', 'Email') }}
-			{{ Form::text('email', Input::old('email'), ['class' => 'form-control', 'placeholder'=>'Enter your email']) }}
-		</div>
+			<div class="{{ ($errors->has('email')) ? 'has-error' : '' }}">
+				{{ $errors->first('email', '<div class="alert alert-danger">:message</div>') }}
+				{{-- {{ Form::label('email', 'Email') }} --}}
+				{{ Form::text('email', Input::old('email'), ['class' => 'form-control form-input email', 'placeholder'=>'Enter your email']) }}
+			</div>
 
-		<div class="form-group {{ ($errors->has('password')) ? 'has-error' : '' }}">
-			{{ $errors->first('password', '<div class="alert alert-danger">:message</div>') }}
-			{{ Form::label('password', 'Password') }}
-			{{ Form::password('password', ['class' => 'form-control', 'placeholder'=>'Enter your password']) }}
-			<br>
-			{{ Form::submit('submit') }}
-			{{ Form::close() }}
+			<div class="{{ ($errors->has('password')) ? 'has-error' : '' }}">
+				{{ $errors->first('password', '<div class="alert alert-danger">:message</div>') }}
+				{{-- {{ Form::label('password', 'Password') }} --}}
+				{{ Form::password('password', ['class' => 'password form-control form-input', 'placeholder'=>'Enter your password']) }}
+				<br>
+				{{ Form::submit('submit', ['id'=>'button-blue']) }}
+				{{ Form::close() }}
 
+			</div>
 		</div>
 	</div>
 
