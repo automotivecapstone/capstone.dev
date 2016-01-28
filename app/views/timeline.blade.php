b5e7f76c403581ca299bbb7b8a42f56d352f1cf7
MK <mkwarren21@gmail.com>
2016-01-28 12:00:31 -0600

    fixed pagination colors. updated timeline

--- app/views/timeline.blade.php
+++ app/views/timeline.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 <div class="content content-div">
-	<h2 id="profilebreak">New and Noteworthy</h2>
+	<div class = "brownheader">
+	<h2 class = "font-magneto-item" id="profilebreak">new and noteworthy</h2>
+	</div>
+	<div class="row">
 	<div class="col-md-6">
 		<h3>Tutorials</h3>
 		<hr>
@@ -14,17 +17,18 @@
 		@endforeach
 		</ul>
 	</div>
-
-	<div class="col-md-6 offset-col-md-6">
-	    <h3>Q/As</h3>
-	    <hr>	
-	    <ul>
-		@foreach ($user->tags()->get() as $tag)
-			@foreach($tag->qas()->get() as $qa)
-				<li><a href="{{{action('QasController@show', $qa->id)}}}">{{$qa->question}}</a></li>
+	
+		<div class="col-md-6 offset-col-md-6">
+		    <h3>Q/As</h3>
+		    <hr>	
+		    <ul>
+			@foreach ($user->tags()->get() as $tag)
+				@foreach($tag->qas()->get() as $qa)
+					<li><a href="{{{action('QasController@show', $qa->id)}}}">{{$qa->question}}</a></li>
+				@endforeach
 			@endforeach
-		@endforeach
-		</ul>
-	</div> 
+			</ul>
+		</div> 
+	</div>
 </div>
 @stop
