905d9971944fda8eb705498f491d4ed36ede20f1
Crystal Wyrick <crystalwyrick@Crystals-MacBook-Pro.local>
2016-01-29 11:12:02 -0600

    uploaded images/videos are centering on qas/tuts show views

--- app/views/qas/show.blade.php
+++ app/views/qas/show.blade.php
@@ -41,11 +41,7 @@
 
 	@if(Auth::user()== $qa->user)
 	{{ Form::open(array('action' => array('QasController@destroy', $qa->id, 'files' => true), 'method' => 'DELETE')) }}
-		
-		<a href="{{{ action('QasController@edit', $qa->id) }}}" class="btn btn-info">Edit Question</a>
-
-		<button class="delete-button btn btn-danger">Delete</button>
-
+	
 	{{ Form::close() }}
 	@endif
 
@@ -72,32 +68,35 @@
 	</div>
 
 	<h3 class="title-qas-tuts">{{$converter->convertToHtml($qa->question)}}</h3>
-	@if (isset($qa->image))
-		<img src="{{{ $qa->image }}}" class="col-xs-8 qa-image">
-	@endif
 
-	@if (isset($qa->video))
-		<!-- HTML5 video tag -->
-		<video controls="controls" poster="img/demo.jpg" width="640" height="360" class="col-md-8 qa-image">
-	 		{{-- *******Do we need something like this to control/adjust the sizes of files users may upload? ******* --}}
-			{{-- .mp4 file for native playback in IE9+, Firefox, Chrome, Safari and most mobile browsers --}}
-			<source src="{{{ $qa->video }}}" type="video/mp4" />
-			<source src="{{{ $qa->video }}}" type="video/ogg" />
-			<source src="{{{ $qa->video }}}" type="video/webm" />
-			<source src="{{{ $qa->video }}}" type="video/m4v" />
-			<!-- flash fallback for IE6, IE7, IE8 and Opera -->
-			<object type="application/x-shockwave-flash" data="swf/flowplayer-3.2.18.swf" width="640" height="360">
-				<param name="movie" value="swf/flowplayer-3.2.18.swf" />
-				<param name="allowFullScreen" value="true" />
-				<param name="wmode" value="transparent" />
-				<!-- note the encoded path to the image and video files, relative to the .swf! -->
-				<!-- more on that here: http://en.wikipedia.org/wiki/Percent-encoding -->
-				<param name="flashVars" value="config={'playlist':['..%2Fimg%2Fdemo.jpg',{'url':'{{{ $qa->video }}}','autoPlay':false}]}" />
-				<!-- fallback image if flash fails -->
-				<img src="img/demo.jpg" width="640" height="360" title="No Flash found" />
-			</object>
-		</video>
-	@endif
+	<div class="row">
+		@if (isset($qa->image))
+			<img src="{{{ $qa->image }}}" class="col-xs-8 qa-image">
+		@endif
+
+		@if (isset($qa->video))
+			<!-- HTML5 video tag -->
+			<video controls="controls" poster="img/demo.jpg" width="640" height="360" class="col-md-8 qa-image">
+		 		{{-- *******Do we need something like this to control/adjust the sizes of files users may upload? ******* --}}
+				{{-- .mp4 file for native playback in IE9+, Firefox, Chrome, Safari and most mobile browsers --}}
+				<source src="{{{ $qa->video }}}" type="video/mp4" />
+				<source src="{{{ $qa->video }}}" type="video/ogg" />
+				<source src="{{{ $qa->video }}}" type="video/webm" />
+				<source src="{{{ $qa->video }}}" type="video/m4v" />
+				<!-- flash fallback for IE6, IE7, IE8 and Opera -->
+				<object type="application/x-shockwave-flash" data="swf/flowplayer-3.2.18.swf" width="640" height="360">
+					<param name="movie" value="swf/flowplayer-3.2.18.swf" />
+					<param name="allowFullScreen" value="true" />
+					<param name="wmode" value="transparent" />
+					<!-- note the encoded path to the image and video files, relative to the .swf! -->
+					<!-- more on that here: http://en.wikipedia.org/wiki/Percent-encoding -->
+					<param name="flashVars" value="config={'playlist':['..%2Fimg%2Fdemo.jpg',{'url':'{{{ $qa->video }}}','autoPlay':false}]}" />
+					<!-- fallback image if flash fails -->
+					<img src="img/demo.jpg" width="640" height="360" title="No Flash found" />
+				</object>
+			</video>
+		@endif
+	</div>
 </div>
 
 	<blockquote>
